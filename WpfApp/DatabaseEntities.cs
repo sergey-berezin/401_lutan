2b35319b69fff2a8627e08ad3c95d77279109907
Valeriia Lutan <llutan@mail.ru>
2022-12-05 23:13:01 +0300

    Правки к 3 лабе

---
+++ WpfApp/DatabaseEntities.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Windows.Input;
+
+namespace WpfApp
+{
+    class Picture
+    {
+        [Key]
+        public int ImageId { get; set; }
+        public string Path { get; set; }
+        public string Section { get; set; }
+        public string Info { get; set; }
+        //public byte[] Image { get; set; }
+    }
+
+    class PictureHash
+    {
+        [Key]
+        [ForeignKey(nameof(Picture))]
+        public int ImageId { get; set; }
+        public string Hash { get; set; }
+        public byte[]? Image { get; set; }
+    }
+}
